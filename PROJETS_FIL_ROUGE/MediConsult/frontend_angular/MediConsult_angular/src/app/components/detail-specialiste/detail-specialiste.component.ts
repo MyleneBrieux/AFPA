c4e47c74ba4ca74dc76f79279f8d5b1e7d031db0
mylen <mylene.brieux@gmail.com>
2021-02-02 14:48:47 +0100

    Ajout d'un spinner dans le détail spécialiste

--- PROJETS_FIL_ROUGE/MediConsult/frontend_angular/MediConsult_angular/src/app/components/detail-specialiste/detail-specialiste.component.ts
+++ PROJETS_FIL_ROUGE/MediConsult/frontend_angular/MediConsult_angular/src/app/components/detail-specialiste/detail-specialiste.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SpecialisteService } from '../../services/specialiste.service';
 import { SpecialisteModele } from '../../modeles/specialiste.modele';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-detail-specialiste',
@@ -12,13 +13,19 @@ export class DetailSpecialisteComponent implements OnInit {
 
   specialiste:any;
 
-  constructor(private activatedRoute: ActivatedRoute, private specialisteService: SpecialisteService) {
+  constructor(private activatedRoute: ActivatedRoute, private specialisteService: SpecialisteService, private SpinnerService: NgxSpinnerService) {
   }
 
   ngOnInit() : void {
+    this.getDetailsOfSpecialiste();
+  }
+
+  getDetailsOfSpecialiste(){
+    this.SpinnerService.show();
     const idSpecialiste=this.activatedRoute.snapshot.params['id'];
     this.specialisteService.getSpecialisteById(idSpecialiste).subscribe((response) => {
       this.specialiste = response.body;
+      this.SpinnerService.hide();
     }, (error) => {
       console.log(error);
     });
