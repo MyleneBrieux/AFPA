d97760f290eb00e626d6c4735b4aac489b7f605b
mylen <mylene.brieux@gmail.com>
2021-02-09 15:52:28 +0100

    Suite de la gestion des erreurs formulaire d'inscription

--- PROJETS_FIL_ROUGE/MediConsult/frontend_angular/MediConsult_angular/src/app/components/inscription/inscription.component.ts
+++ PROJETS_FIL_ROUGE/MediConsult/frontend_angular/MediConsult_angular/src/app/components/inscription/inscription.component.ts
@@ -27,64 +27,68 @@ export class InscriptionComponent implements OnInit {
   ok: boolean = false ;
   firstPassword: string;
   confirmPassword: string;
+  vide: boolean = true;
+
   pattern: string | RegExp;
+
 
   constructor(private patientService : PatientService, private specialisteService : SpecialisteService, private router : Router, private SpinnerService: NgxSpinnerService) { }
 
   ngOnInit(): void {
   }
 
+
   getProfilUser(event : Event){
     this.getProfil = (<HTMLInputElement>event.target).value
   }
 
-inscription(){
-  if(this.getProfil == "patient"){
-    this.SpinnerService.show();
-    this.form={
-      nom:this.nom,
-      prenom:this.prenom,
-      age:this.age,
-      email: this.email,
-      password: this.password
+  inscription(){
+    if(this.getProfil == "patient"){
+      this.SpinnerService.show();
+      this.form={
+        nom:this.nom,
+        prenom:this.prenom,
+        age:this.age,
+        email: this.email,
+        password: this.password
+      }
+      this.patientService.addPatient(this.form).subscribe(
+        data => {
+          this.router.navigate(['/specialistes']);
+        }, error => {
+          console.log(error);
+        })
     }
-    this.patientService.addPatient(this.form).subscribe(
-      data => {
-        this.router.navigate(['/specialistes']);
+    if(this.getProfil == "specialiste"){
+      this.SpinnerService.show();
+      this.form={
+        nom:this.nom,
+        prenom:this.prenom,
+        adresse:this.adresse,
+        specialite:this.specialite,
+        email: this.email,
+        password: this.password
+      }
+      this.specialisteService.addSpecialiste(this.form).subscribe(
+        data => {
+          this.router.navigate(['/patients']);
       }, error => {
         console.log(error);
       })
-  }
-  if(this.getProfil == "specialiste"){
-    this.SpinnerService.show();
-    this.form={
-      nom:this.nom,
-      prenom:this.prenom,
-      adresse:this.adresse,
-      specialite:this.specialite,
-      email: this.email,
-      password: this.password
     }
-    this.specialisteService.addSpecialiste(this.form).subscribe(
-      data => {
-        this.router.navigate(['/patients']);
-    }, error => {
-      console.log(error);
-    })
   }
-}
 
-registerPassword(eventEnregistrer){
-  this.firstPassword = (<HTMLInputElement>eventEnregistrer.target).value;
-}
-
-confirmPasswords(eventConfirmer){
-  this.confirmPassword = (<HTMLInputElement>eventConfirmer.target).value;
-  this.ok = false;
-  if(this.confirmPassword===this.firstPassword){
-    this.ok = true;
+  registerPassword(eventEnregistrer){
+    this.firstPassword = (<HTMLInputElement>eventEnregistrer.target).value;
   }
-}
+
+  confirmPasswords(eventConfirmer){
+    this.confirmPassword = (<HTMLInputElement>eventConfirmer.target).value;
+    this.ok = false;
+    if(this.confirmPassword === this.firstPassword){
+      this.ok = true;
+    }
+  }
 
 
 }
